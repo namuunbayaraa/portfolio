71a129e5dc1e651730420146fd75e83e11aec548
Namuun Bayaraa <namuka@gmail.com>
2025-03-13 21:38:19 -0700

    [Fix] Deployment, env (#9)

--- src/notion.js
+++ src/notion.js
@@ -7,11 +7,16 @@ dotenv.config();
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
-
+console.log("NOTION_DATABASE_ID:", import.meta.env.NOTION_DATABASE_ID);
 export async function queryDatabase() {
   try {
     const response = await notion.databases.query({
       database_id: import.meta.env.NOTION_DATABASE_ID,
+      headers: {
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
     });
     return response;
   } catch (error) {
