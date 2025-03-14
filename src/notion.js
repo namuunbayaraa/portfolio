a257e9fdaa46e0ed3a75eaf2662ceb85423abee2
Namuun Bayaraa <namuka@gmail.com>
2025-03-13 21:28:35 -0700

    add env and cache

--- src/notion.js
+++ src/notion.js
@@ -12,7 +12,13 @@ export async function queryDatabase() {
   try {
     const response = await notion.databases.query({
       database_id: import.meta.env.NOTION_DATABASE_ID,
+      headers: {
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
     });
+
     return response;
   } catch (error) {
     console.error("Error querying database:", error);
